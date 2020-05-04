c994c85a1662f818bf8f93116242ff9bdeb8bce2
erty358 <55165697+erty358@users.noreply.github.com>
2020-05-03 20:25:52 -0400

    Date update
    using dates instead of months from 1976, also using the date type for the president data, to make both somewhat compatible

--- project.R
+++ project.R
@@ -5,27 +5,26 @@ library(httr)
 library(XML)
 library(lubridate)
 
+yeartodate <- function(year)
+{
+  day1970 <- ((year-1970)*365) + floor((year-1970)/4)
+  day1970 <- day1970 + 320
+}
+
 partyPresidentTable <- read.table("https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/42MVDX/MFU99O", sep="\t", header=TRUE)
 
-partyPresidentTable <- partyPresidentTable %>% group_by(year, party) %>% summarise(candidatevotes = sum(candidatevotes), totalvotes = sum(totalvotes))
+partyPresidentTable <- partyPresidentTable %>% mutate(date = as_date(yeartodate(year))) %>% select(-year) %>% group_by(date, party) %>% summarise(candidatevotes = sum(candidatevotes), totalvotes = sum(totalvotes))
 partyNames <- partyPresidentTable %>% group_by(party) %>% select(party) %>% distinct()
 consolidatedPresidentTableDem <- partyPresidentTable %>% filter(party == "democrat") %>% rename(demvotes = candidatevotes) %>% select(-party)
 consolidatedPresidentTableRep <- partyPresidentTable %>% filter(party == "republican") %>% rename(repvotes = candidatevotes) %>% select(-party)
 
-consolidatedPresidentTableRep$year <- consolidatedPresidentTableDem$year
+#consolidatedPresidentTableRep$year <- consolidatedPresidentTableDem$year
 
-consolidatedPresidentTable <- full_join(consolidatedPresidentTableDem, consolidatedPresidentTableRep, by = "year") %>% select(-totalvotes.x) %>% rename(totalvotes = totalvotes.y) %>% mutate(thirdpartyvotes = totalvotes - demvotes - repvotes)
+consolidatedPresidentTable <- full_join(consolidatedPresidentTableDem, consolidatedPresidentTableRep, by = "date") %>% select(-totalvotes.x) %>% rename(totalvotes = totalvotes.y) %>% mutate(thirdpartyvotes = totalvotes - demvotes - repvotes)
 
-ggplot(data = consolidatedPresidentTable, aes(x = year)) + geom_line(aes(y = repvotes, color = "darkred")) + geom_line(aes(y = thirdpartyvotes, color = "green")) + geom_line(aes(y = demvotes, color = "steelblue")) + scale_x_continuous(name="Year", breaks = c(1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016)) + scale_y_continuous(name="Votes", limits=c(100000, 70000000)) + labs(title ="Votes Each Election", x = "Year", y = "Votes")
+ggplot(data = consolidatedPresidentTable, aes(x = date)) + geom_line(aes(y = repvotes, color = "darkred")) + geom_line(aes(y = thirdpartyvotes, color = "green")) + geom_line(aes(y = demvotes, color = "steelblue")) + labs(title ="Votes Each Election", x = "Year", y = "Votes")
 
 #ggplot(data = partyTable, aes(x = year, y = candidatevotes, fill = party)) + geom_line(aes(x = year, y = candidatevotes, fill = party)) + scale_x_continuous(name="Year", breaks = c(1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016)) + scale_y_continuous(name="Votes", limits=c(100000, 70000000)) + labs(title ="Votes Each Election", x = "Year", y = "Votes")
-
-#partyAlignmentTable <- read.csv("https://assets.pewresearch.org/wp-content/uploads/sites/5/2016/09/09-13-16-Party-ID-Detailed-Tables.csv")
-
-monthFixer <- function(vector)
-{
-  vector <- as.vector(match(vector, month.abb))
-}
 
 writ <- GET(url = "https://news.gallup.com/poll/15370/party-affiliation.aspx")
 writXML <- htmlParse(content(writ, as = "text"))
@@ -57,13 +56,9 @@ gallup <- bind_cols(gallupDat, gallupRep)
 gallup <- bind_cols(gallup, gallupInd)
 gallup <- bind_cols(gallup, gallupDem)
 
-gallup <- gallup %>% mutate(`Republicans %` = as.numeric(as.character(`Republicans %`))) %>% mutate(`Independents %` = as.numeric(as.character(`Independents %`))) %>% mutate(`Democrats %` = as.numeric(as.character(`Democrats %`))) %>% mutate(year = as.numeric(substr(gallup$Date, 0, 4))) %>% mutate(month = substr(gallup$Date, 6, 8))
+gallup <- gallup %>% mutate(`Republicans %` = as.numeric(as.character(`Republicans %`))) %>% mutate(`Independents %` = as.numeric(as.character(`Independents %`))) %>% mutate(`Democrats %` = as.numeric(as.character(`Democrats %`))) %>% mutate(Date = as_date(substr(gallup$Date, 0, str_locate(gallup$Date, "-")[,1]-1))) %>% gather("party", "percent", `Republicans %`, `Independents %`, `Democrats %`)
 
-gallup <- gallup %>% mutate(monNum = monthFixer(gallup$month)) %>% select(-Date, -month) %>% mutate(monthsfrom1976 = 12*(year-1976) + monNum) %>% gather("party", "percent", `Republicans %`, `Independents %`, `Democrats %`)
-
-#gallup <- gallup %>% group_by(Date) %>% summarise(`Republicans %` = mean(`Republicans %`))
-
-ggplot(data = gallup, aes(x = monthsfrom1976, y = percent, color = party)) + geom_line(aes(x = monthsfrom1976, y = percent, color = party)) + scale_x_continuous(name="Year", breaks = c(336, 360, 384, 408, 432, 456, 480, 504, 528, 552)) + scale_y_continuous(name="Percentage", limits=c(0, 50)) + labs(title ="Percentage of Population", x = "Year", y = "Percentage")
+ggplot(data = gallup, aes(x = Date, y = percent, color = party)) + geom_line(aes(x = Date, y = percent, color = party)) + scale_y_continuous(name="Percentage", limits=c(0, 50)) + labs(title ="Percentage of Population", x = "Year", y = "Percentage")
 
 #gallup <- gallup %>% mutate(Date2 = as.Date(gallup$Date))
 
